9fd18d191fa206cf7d2ffd07346b4c73dc46833d
anton <121177791+Toenns@users.noreply.github.com>
2026-03-26 00:07:32 +0100

    Update PVAnlage.py

--- PVAnlage.py
+++ PVAnlage.py
@@ -43,8 +43,8 @@ def generiere_pv_ertrag(
             df_cache = pd.read_csv(cache_file, sep=';', decimal=',', index_col=0, parse_dates=True)
             #print(df_cache)
             # Zeitzone sicherstellen, falls durch CSV-Export verloren gegangen
-            if df_cache.index.tz is None:
-                df_cache.index = df_cache.index.tz_localize('Europe/Berlin', ambiguous='infer', nonexistent='shift_forward')
+            #if df_cache.index.tz is None:
+            #    df_cache.index = df_cache.index.tz_localize('Europe/Berlin', ambiguous='infer', nonexistent='shift_forward')
             return df_cache[['ertrag_kwh']]
         except Exception as e:
             print(f"Cache konnte nicht gelesen werden, lade Daten neu: {e}")

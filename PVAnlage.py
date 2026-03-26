a8e46dcad3c34566b61e5289d24e5c219172bed4
anton <121177791+Toenns@users.noreply.github.com>
2026-03-26 01:08:51 +0100

    Update PVAnlage.py

--- PVAnlage.py
+++ PVAnlage.py
@@ -106,7 +106,7 @@ def generiere_pv_ertrag(
         start='2025-01-01 00:00:00', 
         periods=35040, 
         freq='15min', 
-        tz='None'
+        tz='Europe/Berlin'
     )
     df_export = df_2025_berlin[['ertrag_kwh']].reindex(ziel_index).fillna(0.0)
     df_export.index = df_export.index.tz_localize(None)
